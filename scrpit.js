2423b44ca0aa87049f25e8b3075fa2e9e03842a0
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-24 04:44:50 +0500

    update js

--- scrpit.js
+++ scrpit.js
@@ -17,10 +17,23 @@ const term = search.value;
 
 //check for empty 
 if(term.trim()) {
-
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=${trem}')
+.then(res => res.json)
+.then(data =>{
+    Console.log(data);
+    resultHeading.innerHTML = '<h2>search result for '$ {term}':</h2>'
+    if(data.meals === null) {
+        resultHeading.innerHTML =' <p> there are no search .try again </p>';
+    } else {
+mealsEl.innerHTML = data.meals.map(meal => 
+    <div class="meal">
+        <img src="$(meal.strMe)"/>
+        </div>
+    }
+}
 } else {
     alert('please enter a search term');
 }
 }
 //event listenners
-submit.addEventListener('submit',searchMeal);
+submit.addEventListener('submit', searchMeal);
